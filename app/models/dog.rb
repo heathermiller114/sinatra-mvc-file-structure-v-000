aa1e83467cd9dd6cd8dc5793e9c19337ee6ec5f7
Heather Miller <heather.miller114@gmail.com>
2019-01-06 22:34:17 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,16 @@ class Dog
   
   attr_accessor :name, :breed, :age
   
+  @@all = []
   
+  def initialize
+    @name = name
+    @breed = breed
+    @age = age
+  end
+  
+  def all
+    @@all << self
+  end
   
 end
